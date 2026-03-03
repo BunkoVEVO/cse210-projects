5c6f44a083182819fc5944697047e386664a675f
BunkoVEVO <thomashail008@gmail.com>
2026-03-02 18:57:41 -0600

    removed properies

--- prove/Develop04/ReflectingActivity.cs
+++ prove/Develop04/ReflectingActivity.cs
@@ -9,7 +9,6 @@ public class ReflectingActivity : Activity
     public ReflectingActivity() : base()
     {
         SetActivityName("Reflection Activity");
-
         SetActivityDescription("This activity will help you reflect on times in your life when you have shown strength and resilience. This will help you recognize the power you have and how you can use it in other aspects of your life.");
 
         _prompts = new List<string>
